777b7836bf818a9286b360b1b4aa50a974d5fd0d
Hafiz <hafizalimi77@gmail.com>
2024-12-01 17:54:19 +0000

    footer stays put

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,29 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import  Footer  from "./components/Footer";
- import Header from './components/Header';
+import Header from './components/Header';
+import "./global.css"
  
 
 
-const App = () => {
+ const App = () => {
   return (
     <Router>
-      <Header/>
-         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer/>
+      <div id="root">
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+             <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 };
+
 
 export default App;
 
